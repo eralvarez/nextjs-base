02a1469083976e288482b860b2678264f783fd4a
Erick Alvarez <erick.alvarez@live.com>
2025-02-16 01:07:12 -0800

    prettier setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import { Typography } from '@mui/material';
 export default function Home() {
   return (
     <div>
-      <Typography variant="h1" gutterBottom>Home</Typography>
+      <Typography variant="h1" gutterBottom>
+        Home
+      </Typography>
       <Typography gutterBottom>Home</Typography>
-      <Typography variant="button" gutterBottom>Home</Typography>
+      <Typography variant="button" gutterBottom>
+        Home
+      </Typography>
     </div>
   );
 }
